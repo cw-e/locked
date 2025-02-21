53dc09eafd2abcea5ccb591fe57e425e459faa9f
nick-rs <nick0029920@gmail.com>
2025-02-21 01:58:31 +0000

    Added new files.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
     return (
         <div>
-            <h1 className="text-4xl">h1</h1>
+            <h1 className="text-4xl font-bold">h1</h1>
         </div>
     )
 }
